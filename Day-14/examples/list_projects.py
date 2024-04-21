b0113dd1a48e72aa26cfa6ac7ac6d9b54a6f46bd
Censure101 <128888595+Censure101@users.noreply.github.com>
2024-04-21 07:50:45 +0000

    update

--- Day-14/examples/list_projects.py
+++ Day-14/examples/list_projects.py
@@ -1,14 +1,10 @@
-# This code sample uses the 'requests' library:
-# http://docs.python-requests.org
 import requests
 from requests.auth import HTTPBasicAuth
 import json
 
-url = "https://palashdhote.atlassian.net//rest/api/3/project"
-
-API_TOKEN="ATATT3xFfGF01jEFjanpmGgCAQU0b0mVgG5_CUAqnuY3AEpcmMZ3DFskyYgzWEQrfjVJ5k9oKCswt6Pai1HLfLrdACX2UhSfABYxJPoGBfbSuoPFya2HDDR5KbFNgutlYsPSQo0_QQTeGGzQso4AHCKG2smxlRZWmympOrPDb7KP_a7coSkDzJc=6153EF26"
-
-auth = HTTPBasicAuth("palashdhote@gmail.com", API_TOKEN)
+url = "https://palashdhote.atlassian.net/rest/api/3/project"
+api_token="ATATT3xFfGF01jEFjanpmGgCAQU0b0mVgG5_CUAqnuY3AEpcmMZ3DFskyYgzWEQrfjVJ5k9oKCswt6Pai1HLfLrdACX2UhSfABYxJPoGBfbSuoPFya2HDDR5KbFNgutlYsPSQo0_QQTeGGzQso4AHCKG2smxlRZWmympOrPDb7KP_a7coSkDzJc=6153EF26"
+auth = HTTPBasicAuth("palashdhote@gmail.com", api_token)
 
 headers = {
   "Accept": "application/json"
@@ -21,8 +17,4 @@ response = requests.request(
    auth=auth
 )
 
-output = json.loads(response.text)
-
-name = output[0]["name"]
-
-print(name)
+print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))

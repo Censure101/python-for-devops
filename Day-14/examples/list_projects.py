4293c764cc514277b35935c2de23f27f96000d09
Censure101 <128888595+Censure101@users.noreply.github.com>
2024-04-22 06:19:02 +0000

    mm

--- Day-14/examples/list_projects.py
+++ Day-14/examples/list_projects.py
@@ -3,7 +3,7 @@ from requests.auth import HTTPBasicAuth
 import json
 
 url = "https://palashdhote.atlassian.net/rest/api/3/project"
-api_token ="ATATT3xFfGF01jEFjanpmGgCAQU0b0mVgG5_CUAqnuY3AEpcmMZ3DFskyYgzWEQrfjVJ5k9oKCswt6Pai1HLfLrdACX2UhSfABYxJPoGBfbSuoPFya2HDDR5KbFNgutlYsPSQo0_QQTeGGzQso4AHCKG2smxlRZWmympOrPDb7KP_a7coSkDzJc=6153EF26"
+api_token = "ATATT3xFfGF01jEFjanpmGgCAQU0b0mVgG5_CUAqnuY3AEpcmMZ3DFskyYgzWEQrfjVJ5k9oKCswt6Pai1HLfLrdACX2UhSfABYxJPoGBfbSuoPFya2HDDR5KbFNgutlYsPSQo0_QQTeGGzQso4AHCKG2smxlRZWmympOrPDb7KP_a7coSkDzJc=6153EF26"
 auth = HTTPBasicAuth("palashdhote@gmail.com", api_token)
 
 headers = {
